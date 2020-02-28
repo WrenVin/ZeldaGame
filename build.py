e8ee1540fd89539f88e93fb24989687e2b7208a7
Vincent Wren <wrenvin@gmail.com>
2020-02-28 17:31:23 -0600

    Compiled

--- build.py
+++ build.py
@@ -1,1 +1,8 @@
-import compileall
+import cx_Freeze
+executables = [cx_Freeze.Executable('main.py')]
+cx_Freeze.setup(
+    name='ZeldaGame',
+    options = {'build_exe': {'packages':['pygame'],
+                            'include_files':['EasyMap.txt', 'HardMap.txt', 'ModerateMap.txt', 'snd/background.mp3', 'snd/victory.mp3', 'snd/walk.mp3', 'img/character.png', 'img/grass.png', 'img/Overworld.png', 'img/sword.png']}},
+    executables = executables
+                            )
